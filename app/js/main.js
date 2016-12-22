8b560bddc33c5fe123467c5d3b1cb066abb4ffd9
Kirill Bubentsov <kirill.dm6@gmail.com>
2016-12-22 19:17:13 +0300

    finish index

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,33 @@ $(document).ready(function(){
 	if(height<600) {
 		height = 600;
 	}
-	$('header').css({
+	$('.index').css({
 		'minHeight': 0,
 		'maxHeight': 'none',
 		'height': height
 	});
 
-	$("#Carousel").carousel({
-		interval : 5000
+	var topPadding = $('.top').height() + 40;
+	$('.schedule').css(
+		'padding-top', topPadding
+	)
+
+	$(window).on('scroll',function(){
+		var topLine = $('.top-line').height();
+		if($(window).scrollTop()>topLine + 21){
+			$('.nav-block').addClass('fixed filter');
+
+		}
+		else{
+			$('.nav-block').removeClass('fixed filter');
+		}
 	});
+
+	$('.schedule table tr:odd').css('background','rgb(153, 153, 153)');
+	$('.schedule table tr:even').css('background','#fff');
+	$('.price table tr:odd').css('background','rgb(153, 153, 153)');
+	$('.price table tr:even').css('background','#e8e4e1');
+
 });
 
 
